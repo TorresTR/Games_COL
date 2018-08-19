0a1c85b0edb09d8bfc7deb8fd4b1ab710d541051
TorresTR <42515938+TorresTR@users.noreply.github.com>
2018-08-19 09:18:49 -0500

    Proyecto base
    Proyecto base; antes de empezar la migracion

---
+++ Ejercicio_loggin.1/Data/D_user.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
+using System.Configuration;
+
+namespace Data
+{
+    public class D_user
+    {
+
+        public DataTable Login(string UserName, string Clave)
+        {
+            DataTable usuario = new DataTable();
+            NpgsqlConnection conection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("usuarios.f_loggin", conection);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                dataAdapter.SelectCommand.Parameters.Add("_user_name", NpgsqlDbType.Text).Value = UserName;
+                dataAdapter.SelectCommand.Parameters.Add("_clave", NpgsqlDbType.Text).Value = Clave;
+
+                conection.Open();
+                dataAdapter.Fill(usuario);
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+
+            }
+            finally
+            {
+                if (conection != null)
+                {
+                    conection.Close();
+                }
+            }
+            return usuario;
+        }
+
+
+
+
+        public DataTable ObtenerUsuarios()
+        {
+            DataTable usuario = new DataTable();
+            NpgsqlConnection conection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("usuarios.f_obtener_us", conection);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                conection.Open();
+                dataAdapter.Fill(usuario);
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+
+            }
+            finally
+            {
+                if (conection != null)
+                {
+                    conection.Close();
+                }
+            }
+            return usuario;
+        }
+
+
+        public DataTable EliminarUsuarios(int id)
+        {
+            DataTable usuario = new DataTable();
+            NpgsqlConnection conection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("usuarios.f_eliminar_usuario", conection);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                dataAdapter.SelectCommand.Parameters.Add("_id", NpgsqlDbType.Integer).Value = id;
+
+                conection.Open();
+                dataAdapter.Fill(usuario);
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+
+            }
+            finally
+            {
+                if (conection != null)
+                {
+                    conection.Close();
+                }
+            }
+            return usuario;
+        }
+
+        public DataTable EditarUsuarios(int id, string nombre, string pass, string user)
+        {
+            DataTable usuario = new DataTable();
+            NpgsqlConnection conection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("usuarios.f_actualizar_usuario", conection);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                dataAdapter.SelectCommand.Parameters.Add("_id", NpgsqlDbType.Integer).Value = id;
+                dataAdapter.SelectCommand.Parameters.Add("_nombre", NpgsqlDbType.Text).Value = nombre;
+                dataAdapter.SelectCommand.Parameters.Add("_pass", NpgsqlDbType.Text).Value = pass;
+                dataAdapter.SelectCommand.Parameters.Add("_user", NpgsqlDbType.Text).Value = user;
+
+                conection.Open();
+                dataAdapter.Fill(usuario);
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+
+            }
+            finally
+            {
+                if (conection != null)
+                {
+                    conection.Close();
+                }
+            }
+            return usuario;
+        }
+
+        public DataTable ObtenerUs(int id)
+        {
+            DataTable usuario = new DataTable();
+            NpgsqlConnection conection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("usuarios.f_obtener_us_esp", conection);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                dataAdapter.SelectCommand.Parameters.Add("_id", NpgsqlDbType.Integer).Value = id;
+
+                conection.Open();
+                dataAdapter.Fill(usuario);
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+
+            }
+            finally
+            {
+                if (conection != null)
+                {
+                    conection.Close();
+                }
+            }
+            return usuario;
+        }
+
+        public DataTable AgregarUsuarionuev(string nombre, string user, string contra)
+        {
+            DataTable usuario = new DataTable();
+            NpgsqlConnection conection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Postgres"].ConnectionString);
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter("usuarios.f_insertar_usuario", conection);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                dataAdapter.SelectCommand.Parameters.Add("_nombre", NpgsqlDbType.Text).Value = nombre;
+                dataAdapter.SelectCommand.Parameters.Add("_user_name", NpgsqlDbType.Text).Value = user;
+                dataAdapter.SelectCommand.Parameters.Add("_clave", NpgsqlDbType.Text).Value = contra;
+
+                conection.Open();
+                dataAdapter.Fill(usuario);
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+
+            }
+            finally
+            {
+                if (conection != null)
+                {
+                    conection.Close();
+                }
+            }
+            return usuario;
+        }
+
+    }// esta no mover 
+
+
+}
---
+++ Ejercicio_loggin.1/Utilitarios/U_user.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilitarios
+{
+    public class U_user
+    {
+
+        private String url;
+        private Int32 userId;
+        private Int32 rol;
+        private String mensaje;
+        private String Username;
+        private String pass;
+
+        public string Mensaje { get => mensaje; set => mensaje = value; }
+        public int Rol { get => rol; set => rol = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public string Url { get => url; set => url = value; }
+        public string Pass { get => pass; set => pass = value; }
+        public string Username1 { get => Username; set => Username = value; }
+    }
+}
