664849b1c49e862db60274169ce9b45e70f14d58
Jonathan8088 <sosa8088@hotmail.com>
2018-09-04 11:46:26 -0500

    correciones

--- Games_COL_Migracion/Games_COL/Web/Controller/Administrador_verMasPostReportado.aspx.cs
+++ Games_COL_Migracion/Games_COL/Web/Controller/Administrador_verMasPostReportado.aspx.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Logica;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Utilitarios;
 
 public partial class View_Administrador_verMasPostReportado : System.Web.UI.Page
 {
@@ -12,23 +14,21 @@ public partial class View_Administrador_verMasPostReportado : System.Web.UI.Page
     {
         Response.Cache.SetNoStore();
 
-        DAOUsuario dac = new DAOUsuario();
-        EDatosCrearPost doc = new EDatosCrearPost(); 
+        QueryString obQueryString = new QueryString(Request.QueryString);
+        obQueryString = L_encriptadoDesencriptado.DecryptQueryString(obQueryString);
 
-        doc.Id = int.Parse(Request.Params["parametro"]);
-
-        DataTable regis = dac.verpag(doc);
-
+        U_userCrearpost doc = new U_userCrearpost();
+        L_Usercs dac = new L_Usercs();
 
 
+        doc.Id = int.Parse(obQueryString["parametro"].ToString());
+        int x = int.Parse(obQueryString["parametro"].ToString());
 
-        if (regis.Rows.Count > 0)
-        {
+        doc = dac.obtenerPostObservadorreportadomoder(doc);
 
-            LB_contenido.Text = regis.Rows[0]["contenido"].ToString();
-            LB_mostrarAutor.Text = regis.Rows[0]["autor"].ToString();
 
-        }
+        LB_contenido.Text = doc.Contenido1.ToString();
+        LB_mostrarAutor.Text = doc.Autor1.ToString();
     }
 
 
@@ -37,7 +37,19 @@ public partial class View_Administrador_verMasPostReportado : System.Web.UI.Page
 
     protected void BT_volver_Click(object sender, EventArgs e)
     {
-        int b = int.Parse(Request.Params["userid"]);
-        Response.Redirect("Administrador_atencion_bloquer_post.aspx?userid=" + b);
+        QueryString obQueryString = new QueryString(Request.QueryString);
+        obQueryString = L_encriptadoDesencriptado.DecryptQueryString(obQueryString);
+
+        int t = int.Parse(obQueryString["userid"].ToString());
+
+        string dat = t.ToString();
+        obQueryString.Add("userid", dat);
+
+
+        U_user data = new U_user();
+        L_Usercs llamado = new L_Usercs();
+
+        data = llamado.reporteAdminpost();
+        Response.Redirect(data.Link_observador + L_encriptadoDesencriptado.EncryptQueryString(obQueryString).ToString());
     }
 }
